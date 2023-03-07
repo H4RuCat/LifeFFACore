202383ac468c4380265982b38bd235ab5ec537ee
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-03-07 23:53:39 +0900

    ヨシ！

---
+++ src/main/java/lifeffa/org/example/lifeffacore/listener/PlayerKillListener.java
@@ -0,0 +1,17 @@
+package lifeffa.org.example.lifeffacore.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class PlayerKillListener implements Listener {
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+
+//        Player killer = e.getEntity().getKiller();
+//        Player killed = e.getEntity().getPlayer();
+
+    }
+
+}
