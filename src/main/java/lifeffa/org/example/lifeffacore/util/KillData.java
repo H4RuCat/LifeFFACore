d9eb5086f9514d73921737c194c233b0c70a3e65
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-03-08 14:59:37 +0900

    remainを追加

--- src/main/java/lifeffa/org/example/lifeffacore/util/KillData.java
+++ src/main/java/lifeffa/org/example/lifeffacore/util/KillData.java
@@ -5,6 +5,7 @@ public class KillData {
     public int death;
     public int killStreak;
     public int points;
+    public int remain;
 
     public KillData(int kill, int death, int killStreak) {
         this.kill = kill;
