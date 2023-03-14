4445ecba3df5a6ced357fc315109e39a4a300b05
siqlet_ <97619923+H4RuCat@users.noreply.github.com>
2023-03-14 09:27:08 +0900

    ヨシ！

--- src/main/java/lifeffa/org/example/lifeffacore/util/InventoryCheckUtil.java
+++ src/main/java/lifeffa/org/example/lifeffacore/util/InventoryCheckUtil.java
@@ -19,29 +19,46 @@ public class InventoryCheckUtil {
     private final FileConfiguration info = LifeFFACore.inst().getConfig();
 
     public boolean invCheck(@NotNull Player player) {
+
         for (PotionEffectType type: PotionEffectType.values()) {
+
             player.removePotionEffect(type);
+
         }
 
         AttributeInstance health = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-        if (health != null) {
+
+        if ( health != null ) {
+
             for (AttributeModifier modifier: health.getModifiers()) {
-                if (modifier.getName().equals("PlayerBuff.SetHealth")) health.removeModifier(modifier);
+
+                if ( modifier.getName().equals("PlayerBuff.SetHealth") ) health.removeModifier(modifier);
+
             }
+
         }
 
         AttributeInstance armor = player.getAttribute(Attribute.GENERIC_ARMOR);
-        if (armor != null) {
+
+        if ( armor != null ) {
+
             for (AttributeModifier modifier: armor.getModifiers()) {
-                if (modifier.getName().equals("PlayerBuff.SetArmor")) armor.removeModifier(modifier);
+
+                if ( modifier.getName().equals("PlayerBuff.SetArmor") ) armor.removeModifier(modifier);
+
             }
+
         }
 
         AttributeInstance toughness = player.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS);
-        if (toughness != null) {
+        if ( toughness != null ) {
+
             for (AttributeModifier modifier: toughness.getModifiers()) {
-                if (modifier.getName().equals("PlayerBuff.SetArmor_Toughness")) toughness.removeModifier(modifier);
+
+                if ( modifier.getName().equals("PlayerBuff.SetArmor_Toughness") ) toughness.removeModifier(modifier);
+
             }
+
         }
 
         return mmCheck(player) && isHead(player) && isChest(player) && isLeg(player) && isBoots(player) && isOffHand(player);
@@ -89,79 +106,147 @@ public class InventoryCheckUtil {
     }
 
     public boolean isHead(@NotNull Player player) {
+
         ItemStack item = player.getInventory().getHelmet();
-        if (item == null) return true;
+
+        if ( item == null ) return true;
+
         CompoundTag tag = MythicMobs.inst().getVolatileCodeHandler().getItemHandler().getNBTData(item);
-        if (tag != null && tag.containsKey("MYTHIC_TYPE")) {
+
+        if ( tag != null && tag.containsKey("MYTHIC_TYPE") ) {
+
             for (String string : info.getStringList("Armor")) {
+
                 if (tag.getString("MYTHIC_TYPE").equals(string)) return true;
+
             }
+
             player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
             return false;
+
         }
+
         player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
         return false;
+
     }
 
     public boolean isChest(@NotNull Player player) {
+
         ItemStack item = player.getInventory().getChestplate();
-        if (item == null) return true;
+
+        if ( item == null ) return true;
+
         CompoundTag tag = MythicMobs.inst().getVolatileCodeHandler().getItemHandler().getNBTData(item);
-        if (tag != null && tag.containsKey("MYTHIC_TYPE")) {
+
+        if ( tag != null && tag.containsKey("MYTHIC_TYPE") ) {
+
             for (String string : info.getStringList("Armor")) {
-                if (tag.getString("MYTHIC_TYPE").equals(string)) {
+
+                if ( tag.getString("MYTHIC_TYPE").equals(string) ) {
+
                     return true;
+
                 }
+
             }
+
             player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
             return false;
+
         }
+
         player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
         return false;
+
     }
 
     public boolean isLeg(@NotNull Player player) {
+
         ItemStack item = player.getInventory().getLeggings();
+
         if (item == null) return true;
+
         CompoundTag tag = MythicMobs.inst().getVolatileCodeHandler().getItemHandler().getNBTData(item);
-        if (tag != null && tag.containsKey("MYTHIC_TYPE")) {
+
+        if ( tag != null && tag.containsKey("MYTHIC_TYPE") ) {
+
             for (String string : info.getStringList("Armor")) {
-                if (tag.getString("MYTHIC_TYPE").equals(string)) return true;
+
+                if ( tag.getString("MYTHIC_TYPE").equals(string) ) return true;
+
             }
+
             player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
             return false;
+
         }
+
         player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
         return false;
+
     }
 
     public boolean isBoots(@NotNull Player player) {
+
         ItemStack item = player.getInventory().getBoots();
+
         if (item == null) return true;
+
         CompoundTag tag = MythicMobs.inst().getVolatileCodeHandler().getItemHandler().getNBTData(item);
-        if (tag != null && tag.containsKey("MYTHIC_TYPE")) {
+
+        if ( tag != null && tag.containsKey("MYTHIC_TYPE") ) {
+
             for (String string : info.getStringList("Armor")) {
-                if (tag.getString("MYTHIC_TYPE").equals(string)) return true;
+
+                if ( tag.getString("MYTHIC_TYPE").equals(string) ) return true;
+
             }
+
             player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
             return false;
+
         }
+
         player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
         return false;
+
     }
 
     public boolean isOffHand(@NotNull Player player) {
+
         ItemStack item = player.getInventory().getItemInOffHand();
+
         if (item.getType().isAir()) return true;
+
         CompoundTag tag = MythicMobs.inst().getVolatileCodeHandler().getItemHandler().getNBTData(item);
-        if (tag != null && tag.containsKey("MYTHIC_TYPE")) {
+
+        if ( tag != null && tag.containsKey("MYTHIC_TYPE") ) {
+
             for (String string : info.getStringList("Armor")) {
-                if (tag.getString("MYTHIC_TYPE").equals(string)) return true;
+
+                if ( tag.getString("MYTHIC_TYPE").equals(string) ) return true;
+
             }
+
             player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
             return false;
+
         }
+
         player.sendMessage(item.getItemMeta().getDisplayName() + ChatColor.RESET + "はlifeFFAワールド内では使用できません。");
+
         return false;
+
     }
+
 }
